db1bf331e6f9f9ec23c84f2e812a83637aa6c4cf
ammyy9908 <sb78639@gmail.com>
2022-03-16 07:57:14 -0400

    Logical And Operator added

---
+++ logical_operator.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+age=25
+if [ $age -gt 18 ] && [ $age -lt 30 ]
+then 
+    echo "Valid Age"
+else
+    echo "Invalid Age"
+fi
+
+# Second  way of Logical And
+
+
+if [ $age -gt 18 -a $age -lt 30 ]
+then 
+    echo "Valid Age"
+else
+    echo "Invalid Age"
+fi
+
+# Third  way of Logical And
+
+
+if [[ $age -gt 18 && $age -lt 30 ]]
+then 
+    echo "Valid Age"
+else
+    echo "Invalid Age"
+fi
+
